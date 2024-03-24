794536f3eafa84847d9049183ad8e886c665c3af
Priyanshi Bhikadiya <priyanshibhikadiya.dev@gmail.com>
2024-03-24 10:47:46 +0530

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Dharmik Kalathiya on 2024-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
